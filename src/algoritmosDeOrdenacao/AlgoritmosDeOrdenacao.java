5de52c63f679c7da3c43b5f71527727dff28e383
Daniel Marques <danmarquesvg@gmail.com>
2016-07-27 01:03:04 -0300

    Algoritmos de Ordenação
    BubbleSort, Insertion Sort, Merge Sort e Counting Sort

---
+++ src/algoritmosDeOrdenacao/AlgoritmosDeOrdenacao.java
@@ -0,0 +1,119 @@
+package algoritmosDeOrdenacao;
+
+public class AlgoritmosDeOrdenacao {
+
+	// MÉTODO BUBBLE SORT
+	public static int[] bubbleSort(int[] lista) {
+
+		boolean houveTroca = true;
+
+		while (houveTroca == true) {
+			houveTroca = false;
+			for (int i = 0; i < (lista.length) - 1; i++) {
+				if (lista[i] > lista[i + 1]) {
+					int variavelAuxiliar = lista[i + 1];
+					lista[i + 1] = lista[i];
+					lista[i] = variavelAuxiliar;
+					houveTroca = true;
+				}
+			}
+		}
+
+		return lista;
+	}
+
+	// MÉTODO INSERTION SORT
+	public static int[] insertionSort(int[] lista) {
+
+		for (int i = 1; i < lista.length; i++) {
+			int variavelAuxiliar = lista[i];
+			int j = i;
+			while (j > 0 && lista[j - 1] > variavelAuxiliar) {
+				lista[j] = lista[j - 1];
+				j = j - 1;
+			}
+			lista[j] = variavelAuxiliar;
+		}
+
+		return lista;
+
+	}
+
+	// MÉTODO MERGE SORT
+	public int[] mergeSort(int[] arrayEntrada, int p, int r) {
+
+		int q;
+		if (p < r) {
+			q = (int) Math.floor((p + r) / 2);
+			mergeSort(arrayEntrada, p, q);
+			mergeSort(arrayEntrada, q + 1, r);
+			megeSortIntercala(arrayEntrada, p, q, r);
+		}
+		return arrayEntrada;
+
+	}
+	
+
+	public int[] megeSortIntercala(int[] arrayEntrada, int p, int q, int r) {
+
+		int n = q - p + 1;
+		int m = r - q;
+
+		int L[] = new int[n + 1];
+		int R[] = new int[m + 1];
+		int i, j, k;
+
+		for (i = 0; i < n; i++) {
+			L[i] = arrayEntrada[p + i];
+		}
+
+		for (j = 0; j < m; j++) {
+			R[j] = arrayEntrada[q + j + 1];
+		}
+
+		L[n] = Integer.MAX_VALUE;
+		R[m] = Integer.MAX_VALUE;
+
+		i = 0;
+		j = 0;
+		for (k = p; k <= r; k++) {
+
+			if (L[i] <= R[j]) {
+				arrayEntrada[k] = L[i];
+				i = i + 1;
+			} else {
+				arrayEntrada[k] = R[j];
+				j = j + 1;
+			}
+		}
+
+		return arrayEntrada;
+	}
+
+	// MÉTODO COUNTING SORT
+	public int[] countingSort(int[] arrayA, int k) {
+
+		int[] arrayB = new int[arrayA.length];
+		int[] arrayC = new int[k + 1];
+
+		for (int i = 1; i <= k; i++) {
+			arrayC[i] = 0;
+		}
+
+		for (int j = 0; j < arrayA.length; j++) {
+			arrayC[arrayA[j]] = arrayC[arrayA[j]] + 1;
+		}
+
+		for (int i = 1; i < k + 1; i++) {
+			arrayC[i] = arrayC[i] + arrayC[i - 1];
+		}
+
+		for (int j = (arrayA.length - 1); j >= 0; j--) {
+			arrayB[arrayC[arrayA[j]] - 1] = arrayA[j];
+			arrayC[arrayA[j]] = arrayC[arrayA[j]] - 1;
+		}
+		return arrayB;
+
+	}
+
+}
